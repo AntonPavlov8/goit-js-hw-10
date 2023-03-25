b2f2722056de0e5854632a208ef2517a4a2ecee5
AntonPavlov8 <barsikstd@gmail.com>
2023-03-25 13:41:13 -0500

    gg

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,6 @@
 export default function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    console.log(response);
     if (!response.ok) {
       throw new Error(response.status);
     } else return response.json();
